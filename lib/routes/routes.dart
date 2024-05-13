3c3b25b5080645a29b95b50a54d742274f9dbb9a
MahendraKhibrah <mahendra.khibrah@socialbread.id>
2024-05-14 00:36:28 +0700

    add custom splash screen

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -3,13 +3,14 @@ import 'package:toefl/pages/full_test/full_test_page.dart';
 import 'package:toefl/pages/login_page.dart';
 import 'package:toefl/pages/regist_page.dart';
 import 'package:toefl/pages/games_page.dart';
+import 'package:toefl/pages/splash_page.dart';
 import 'package:toefl/pages/template_page.dart';
 import 'package:toefl/routes/route_key.dart';
 import 'package:toefl/pages/main_page.dart';
 
 final routes = <String, Widget Function(BuildContext)>{
   // TODO : CHANGE ROOT TO SPLASH PAGE
-  RouteKey.root: (context) => const MainPage(),
+  RouteKey.root: (context) => const SplashPage(),
   RouteKey.main: (context) => const MainPage(),
   RouteKey.fullTest: (context) => const FullTestPage(),
   RouteKey.regist: (context) => const RegistPage(),
