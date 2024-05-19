c600e97248607ffe93a4c471e996f8bb4fa81f3b
MahendraKhibrah <mahendra.khibrah@socialbread.id>
2024-05-19 12:00:27 +0700

    api integration of set target page

--- lib/pages/full_test/set_target_page.dart
+++ lib/pages/full_test/set_target_page.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:toefl/models/test/test_target.dart';
+import 'package:toefl/remote/api/user_api.dart';
 import 'package:toefl/utils/colors.dart';
 import 'package:toefl/utils/custom_text_style.dart';
 import 'package:toefl/utils/hex_color.dart';
@@ -13,20 +16,51 @@ class SetTargetPage extends StatefulWidget {
 }
 
 class _SetTargetPageState extends State<SetTargetPage> {
-  final List<Map<String, String>> items = [
-    {"containerText": "199", "labelText": "Elementary Proficiency"},
-    {"containerText": "350", "labelText": "Pre-Intermediate Proficiency"},
-    {"containerText": "425", "labelText": "Intermediate Proficiency"},
-    {"containerText": "500", "labelText": "Pre-Advanced Proficiency"},
-    {"containerText": "550", "labelText": "Advance Proficiency"},
-    {"containerText": "660", "labelText": "Special Advance Proficiency"},
-  ];
+  TestTarget selectedTarget = TestTarget(
+    id: "",
+    name: "",
+    score: 0,
+  );
+
+  List<TestTarget> allTargets = [];
+  UserApi api = UserApi();
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _onInit();
+  }
+
+  void _onInit() async {
+    setState(() {
+      isLoading = true;
+    });
+    try {
+      UserTarget userTarget = await api.getUserTarget();
+      setState(() {
+        selectedTarget = userTarget.selectedTarget;
+        allTargets = userTarget.allTargets;
+      });
+      debugPrint("selectedTarget: ${selectedTarget.id}");
+    } catch (e) {
+      debugPrint("Error in _onInit: $e");
+    } finally {
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: Icon(Icons.arrow_back),
+        leading: GestureDetector(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: const Icon(Icons.arrow_back)),
       ),
       body: Column(
         children: [
@@ -41,11 +75,18 @@ class _SetTargetPageState extends State<SetTargetPage> {
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 30),
               child: ListView.builder(
-                itemCount: items.length,
+                itemCount: allTargets.length,
                 itemBuilder: (context, index) {
                   return CustomListTile(
-                    containerText: items[index]["containerText"]!,
-                    labelText: items[index]["labelText"]!,
+                    containerText: allTargets[index].score.toString(),
+                    labelText: allTargets[index].name,
+                    isSelected: selectedTarget.id == allTargets[index].id,
+                    onTap: () async {
+                      setState(() {
+                        selectedTarget = allTargets[index];
+                      });
+                      await api.updateBookmark(selectedTarget.id);
+                    },
                   );
                 },
               ),
@@ -107,32 +148,62 @@ class CustomContainerText extends StatelessWidget {
 class CustomListTile extends StatelessWidget {
   final String containerText;
   final String labelText;
+  final bool isSelected;
+  final Function() onTap;
 
   const CustomListTile({
     Key? key,
     required this.containerText,
     required this.labelText,
+    this.isSelected = false,
+    required this.onTap,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: BlueContainer(
-        child: ListTile(
-          leading: CustomContainerText(
-            text: containerText,
-            width: 50,
-            height: 50,
-            containerColor:
-                HexColor(mariner700), // Ganti dengan warna yang diinginkan
-            textStyle: CustomTextStyle.bold18.copyWith(color: Colors.white),
-            borderRadius: BorderRadius.circular(25),
-          ),
-          title: Text(
-            labelText,
-            style: CustomTextStyle.medium15.copyWith(color: Colors.black),
-          ),
+      child: GestureDetector(
+        onTap: () {
+          if (isSelected) {
+            debugPrint("Already selected");
+            return;
+          } else {
+            debugPrint("Not selected yet");
+            onTap();
+          }
+        },
+        child: Stack(
+          children: [
+            BlueContainer(
+              showShadow: isSelected,
+              child: ListTile(
+                leading: CustomContainerText(
+                  text: containerText,
+                  width: 50,
+                  height: 50,
+                  containerColor: HexColor(mariner700),
+                  textStyle:
+                      CustomTextStyle.bold18.copyWith(color: Colors.white),
+                  borderRadius: BorderRadius.circular(25),
+                ),
+                title: Text(
+                  labelText,
+                  style: CustomTextStyle.medium15.copyWith(color: Colors.black),
+                ),
+              ),
+            ),
+            Positioned.fill(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: !isSelected
+                      ? HexColor(neutral40).withOpacity(0.3)
+                      : Colors.transparent,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
