a5b3298c03f50ebf5746bb820e27d67b64b4944b
Reza1290 <reza.muktasib@gmail.com>
2024-05-27 00:27:34 +0700

    add: learning path api

---
+++ lib/remote/api/learning_path_api.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+import 'package:toefl/models/home_page/learning_path_model.dart';
+import 'package:toefl/models/quiz.dart';
+import 'package:toefl/remote/base_response.dart';
+import 'package:toefl/remote/dio_toefl.dart';
+import 'package:toefl/remote/env.dart';
+
+class LearningPathApi {
+  Future<List<LearningPathModel>> getLearning() async {
+    try {
+      final Response rawResponse =
+          await DioToefl.instance.get('${Env.apiUrl}/quiztypes');
+      final response = BaseResponse.fromJson(json.decode(rawResponse.data));
+      final List<dynamic> dataTypes = response.data;
+      List<LearningPathModel> types =
+          dataTypes.map((e) => LearningPathModel.fromJson(e)).toList();
+      return types;
+    } catch (e) {
+      return [];
+    }
+  }
+
+  Future<List<Quiz>> getQuizByCategory(String id) async {
+    try {
+      final Response rawResponse =
+          await DioToefl.instance.get('${Env.apiUrl}/quiztypes/$id');
+      final response = BaseResponse.fromJson(json.decode(rawResponse.data));
+      final List<dynamic> dataTypes = response.data;
+      List<Quiz> quizzes = dataTypes.map((e) => Quiz.fromJson(e)).toList();
+      return quizzes;
+    } catch (e) {
+      return [];
+    }
+  }
+}
