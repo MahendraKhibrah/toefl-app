fb9bfa3a9fe8570da443f87fc895abdb2f441371
MahendraKhibrah <mahendra.khibrah@socialbread.id>
2024-05-26 11:20:24 +0700

    add change language dialog

--- lib/widgets/profile_page/settings.dart
+++ lib/widgets/profile_page/settings.dart
@@ -7,6 +7,7 @@ import 'package:toefl/remote/local/sqlite/mini_test_table.dart';
 import 'package:toefl/routes/route_key.dart';
 import 'package:toefl/utils/colors.dart';
 import 'package:toefl/utils/hex_color.dart';
+import 'package:toefl/widgets/profile_page/change_lang_dialog.dart';
 
 class Setting extends StatefulWidget {
   const Setting({Key? key}) : super(key: key);
@@ -30,23 +31,23 @@ class _SettingState extends State<Setting> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 20),
+      padding: const EdgeInsets.symmetric(horizontal: 20),
       child: Column(
         children: [
           _listTileCustom(Icons.notifications, 'Notification',
               trailing: _switchButton()),
-          Divider(
+          const Divider(
             color: Color(0xffE7E7E7),
             thickness: 1,
           ),
           _listTileCustom(Icons.g_translate, 'Your Language',
               trailing: _dropdownButton()),
-          Divider(
+          const Divider(
             color: Color(0xffE7E7E7),
             thickness: 1,
           ),
           InkWell(
-            splashColor: Color(0xffE7E7E7).withOpacity(0.3),
+            splashColor: const Color(0xffE7E7E7).withOpacity(0.3),
             highlightColor: Colors.transparent,
             onTap: () {
               Navigator.pushNamed(context, RouteKey.setTargetPage);
@@ -54,12 +55,12 @@ class _SettingState extends State<Setting> {
             child:
                 _listTileCustom(Icons.bar_chart_rounded, 'Set my TOEFL Target'),
           ),
-          Divider(
+          const Divider(
             color: Color(0xffE7E7E7),
             thickness: 1,
           ),
           InkWell(
-            splashColor: Color(0xffE7E7E7).withOpacity(0.3),
+            splashColor: const Color(0xffE7E7E7).withOpacity(0.3),
             highlightColor: Colors.transparent,
             onTap: () async {
               await authSharedPreference.removeBearerToken();
@@ -80,16 +81,16 @@ class _SettingState extends State<Setting> {
   Widget _listTileCustom(IconData icon, String text,
       {Widget? trailing, bool islogout = false}) {
     return ListTile(
-      contentPadding: EdgeInsets.all(0),
+      contentPadding: const EdgeInsets.all(0),
       leading: Icon(
         icon,
-        color: (islogout ? Color(0xffF44336) : HexColor(mariner800)),
+        color: (islogout ? const Color(0xffF44336) : HexColor(mariner800)),
       ),
       title: Text(
         text,
         style: TextStyle(
           fontSize: 14,
-          color: (islogout ? Color(0xffF44336) : Color(0xff191919)),
+          color: (islogout ? const Color(0xffF44336) : const Color(0xff191919)),
         ),
       ),
       trailing: trailing,
@@ -101,19 +102,19 @@ class _SettingState extends State<Setting> {
       height: 22,
       decoration: BoxDecoration(
         border: Border.all(color: Colors.transparent),
-        borderRadius: BorderRadius.all(Radius.circular(15)),
-        color: Color(0xffF6F6F6),
+        borderRadius: const BorderRadius.all(Radius.circular(15)),
+        color: const Color(0xffF6F6F6),
         boxShadow: [
           BoxShadow(
             color: Colors.grey.withOpacity(0.2),
             spreadRadius: 0,
-            offset: Offset(0, -2),
+            offset: const Offset(0, -2),
           ),
         ],
       ),
       child: DropdownButton(
         borderRadius: BorderRadius.circular(15),
-        padding: EdgeInsets.symmetric(horizontal: 15),
+        padding: const EdgeInsets.symmetric(horizontal: 15),
         value: dropdownvalue,
         icon: const Icon(
           Icons.keyboard_arrow_down,
@@ -125,7 +126,7 @@ class _SettingState extends State<Setting> {
             value: items,
             child: Text(
               items,
-              style: TextStyle(
+              style: const TextStyle(
                   fontWeight: FontWeight.w600,
                   fontSize: 10,
                   color: Color(0xff191919)),
@@ -133,9 +134,26 @@ class _SettingState extends State<Setting> {
           );
         }).toList(),
         onChanged: (String? newValue) {
-          setState(() {
-            dropdownvalue = newValue!;
-          });
+          showDialog(
+            context: context,
+            builder: (BuildContext dialogContext) {
+              return AlertDialog(
+                  backgroundColor: Colors.transparent,
+                  contentPadding:
+                      const EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+                  content: ChangeLangDialog(
+                    onNo: () {
+                      Navigator.of(dialogContext).pop();
+                    },
+                    onYes: () {
+                      Navigator.of(dialogContext).pop();
+                    },
+                  ));
+            },
+          );
+          // setState(() {
+          //   dropdownvalue = newValue!;
+          // });
         },
         underline: Container(),
       ),
