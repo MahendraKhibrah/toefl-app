abe35f45bb22dc0f4a710f948ff457b672b0ff16
shofirain <shofiranurrohmah@gmail.com>
2024-06-06 09:39:55 +0700

    terbaru

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -8,11 +8,8 @@
 
 #include <flutter_tts/flutter_tts_plugin.h>
 #include <local_auth_windows/local_auth_plugin.h>
-<<<<<<< HEAD
 #include <permission_handler_windows/permission_handler_windows_plugin.h>
-=======
 #include <share_plus/share_plus_windows_plugin_c_api.h>
->>>>>>> ac0ba1eec9ab9fd18571c4f11d1257788c2166d1
 #include <url_launcher_windows/url_launcher_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
@@ -20,13 +17,10 @@ void RegisterPlugins(flutter::PluginRegistry* registry) {
       registry->GetRegistrarForPlugin("FlutterTtsPlugin"));
   LocalAuthPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("LocalAuthPlugin"));
-<<<<<<< HEAD
   PermissionHandlerWindowsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("PermissionHandlerWindowsPlugin"));
-=======
   SharePlusWindowsPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("SharePlusWindowsPluginCApi"));
->>>>>>> ac0ba1eec9ab9fd18571c4f11d1257788c2166d1
   UrlLauncherWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("UrlLauncherWindows"));
 }
